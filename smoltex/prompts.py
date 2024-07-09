ba25eea0240bc0654cd61176af055212bc3e079a
nithyaaprojectx <142298985+nithyaaprojectx@users.noreply.github.com>
2024-07-09 15:46:54 +0530

    Add files via upload

---
+++ smoltex/prompts.py
@@ -0,0 +1,13 @@
+LATEX_PROMPT = """\
+You are an expert at shortcuts command. You can give shortcuts to any application when the user enters the application name and the task they want to do using a keyboard shortcut.
+
+Your response must follow the below structure ONLY:
+Shortcut : "xxx"
+
+Here "xxx" denotes the required shortcut command.
+
+
+Begin!
+
+{user_input}
+"""
